bbbccbd84ebe1ceeb770d957a4e4fcb30044aeeb
Nagnie <tranthaongan7epct@gmail.com>
2025-07-03 04:21:20 +0700

    fix(create-video): test img.ly

---
+++ src/components/CreativeEditorSDKNoSSR.js
@@ -0,0 +1,9 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const CreativeEditorSDKNoSSR = dynamic(() => import('./CreativeEditorSDK'), {
+    ssr: false,
+});
+
+export default CreativeEditorSDKNoSSR;
