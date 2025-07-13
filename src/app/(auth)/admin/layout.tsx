20b53f1603071eb76ff51db653f1fada46566083
22120217 <hoanglenam0905@gmail.com>
2025-07-13 08:39:55 +0700

    git commit -m "feat(admin): add admin auth layout for admin pages"

---
+++ src/app/(auth)/admin/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/option";
+import { redirect, RedirectType } from "next/navigation";
+import { AuthProvider } from "@/app/(auth)/user/_components/auth-provider";
+
+export default async function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const session = await getServerSession(authOptions);
+
+    // kiểm tra session có tồn tại hay không
+    // nếu có redirect về trang home
+    if (session && session.accessToken) {
+        // Redirect to home if session exists
+        redirect("/", RedirectType.replace);
+    }
+
+    return (
+        <main className="container mx-auto !bg-[#0A0A0A] !w-[100vw] flex flex-col min-h-screen">
+            <AuthProvider session={session}>{children}</AuthProvider>
+            <Toaster />
+        </main>
+    );
+}
